732dffadaf4f4b8044fded69afa384bfc0b2cc48
MUKESH <mukeshsinghjsr1995@gmail.com>
2025-03-21 23:26:03 +0530

    changes made

---
+++ moderate/firstjenkin/first.java
@@ -0,0 +1,10 @@
+public class StudentNames {
+    public static void main(String[] args) {
+        String[] students = {"Alice", "Bob", "Charlie", "David", "Eva"};
+
+        System.out.println("Student Names:");
+        for (String student : students) {
+            System.out.println(student);
+        }
+    }
+}
